e8423ea77b17808a6f612507b5ab48f7a241bfd3
CLSnapp <repbandito@hotmail.com>
2025-02-12 13:57:42 -0600

    completed and tested register/login/me routes and updated the user's schema

---
+++ server/api/comments.js
@@ -0,0 +1,5 @@
+// import React from "react";
+
+// export default function comments() {
+//   return <div>comments</div>;
+// }
---
+++ server/db/seed.js
@@ -0,0 +1,73 @@
+const db = require("../db/index");
+const { faker } = require("@faker-js/faker");
+const { prisma } = require("../db/common");
+require("dotenv").config();
+
+async function seed() {
+  console.log("Seeding the database.");
+  try {
+    await db.query("DROP TABLE IF EXISTS users, items, comments, reviews;");
+
+    // Add 10 Users.
+    await Promise.all(
+      [...Array(10)].map(() =>
+        prisma.users.create({
+          data: {
+            username: faker.internet.username(),
+            password: faker.internet.password(),
+          },
+        })
+      )
+    );
+
+    // Add 20 Recipes.
+    await Promise.all(
+      [...Array(20)].map(() =>
+        prisma.items.createMany({
+          data: {
+            name: faker.commerce.product(),
+            description: faker.commerce.productDescription(),
+          },
+        })
+      )
+    );
+
+    // Add 10 Reviews.
+    await Promise.all(
+      [...Array(10)].map((_, i) =>
+        prisma.reviews.createMany({
+          data: {
+            review: faker.lorem.paragraph(2),
+            rating: faker.number.int(5),
+            userId: (i % 5) + 1,
+            itemId: (i % 5) + 1,
+          },
+        })
+      )
+    );
+
+    // Add 10 Comments.
+    await Promise.all(
+      [...Array(10)].map((_, i) =>
+        prisma.comments.createMany({
+          data: {
+            comment: faker.lorem.paragraph(2),
+            userId: (i % 5) + 1,
+            reviewId: (i % 5) + 1,
+          },
+        })
+      )
+    );
+
+    console.log("Database is seeded.");
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+// Seed the database if we are running this file directly.
+if (require.main === module) {
+  seed();
+}
+
+module.exports = seed;
