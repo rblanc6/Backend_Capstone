7d039960a5a585425f0dcd51109d850410538705
CLSnapp <repbandito@hotmail.com>
2025-02-12 17:29:34 -0600

    added admin api routes, admin role

--- server/api/index.js
+++ server/api/index.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 
 //Import Backend routes
 router.use("/auth", require("./auth"));
+router.use("/admin", require("./admin"));
 // router.use("/recipes", require("./recipes"));
 // router.use("/comments", require("./comments"));
 // router.use("/reviews", require("./reviews.js"));
